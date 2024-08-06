e6aec1b35cf33cf8c433c949dc18be9d67919624
mosayed01 <asdas2018888@gmail.com>
2024-08-07 01:03:29 +0300

    add test cases

---
+++ src/test/java/com/example/demo/ciphers/des/logic/utils/HexStringTest.java
@@ -0,0 +1,42 @@
+package com.example.demo.ciphers.des.logic.utils;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HexStringTest {
+
+    @Test
+    void toBinaryByteArray() {
+        // given a hex string
+        String hexString = "0123456789ABCDEF";
+        // when converting to binary byte array
+        HexString hexStringObj = new HexString(hexString);
+        byte[] binaryByteArray = hexStringObj.toBinaryByteArray();
+        // then return the correct binary byte array
+        String binaryString = "0000000100100011010001010110011110001001101010111100110111101111";
+        byte[] trueResult = new byte[binaryString.length()];
+
+        for (int i = 0; i < binaryString.length(); i++) {
+            trueResult[i] = (byte) (binaryString.charAt(i) - '0');
+        }
+        assertArrayEquals(trueResult, binaryByteArray);
+    }
+
+    @Test
+    void fromBinaryByteArray() {
+        // given a binary byte array
+        String binaryString = "0000000100100011010001010110011110001001101010111100110111101111";
+        byte[] binaryByteArray = new byte[binaryString.length()];
+
+        for (int i = 0; i < binaryString.length(); i++) {
+            binaryByteArray[i] = (byte) (binaryString.charAt(i) - '0');
+        }
+        // when converting to hex string
+        HexString hexStringObj = HexString.fromBinaryByteArray(binaryByteArray);
+        // then return the correct hex string
+        String trueResult = "0123456789ABCDEF";
+        assertEquals(trueResult, hexStringObj.toString());
+    }
+}
---
+++ src/test/java/com/example/demo/ciphers/des/logic/utils/KeyScheduleTest.java
@@ -0,0 +1,40 @@
+package com.example.demo.ciphers.des.logic.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KeyScheduleTest {
+
+    @Test
+    void generateSubKeys() {
+        // given a key
+        String hexKey = "AABB09182736CCDD";
+        // when generating sub keys
+        byte[][] subKeys = KeySchedule.generateSubKeys(hexKey);
+        // then return the correct sub keys
+        String[] trueResults = {
+                "194CD072DE8C",
+                "4568581ABCCE",
+                "06EDA4ACF5B5",
+                "DA2D032B6EE3",
+                "69A629FEC913",
+                "C1948E87475E",
+                "708AD2DDB3C0",
+                "34F822F0C66D",
+                "84BB4473DCCC",
+                "02765708B5BF",
+                "6D5560AF7CA5",
+                "C2C1E96A4BF3",
+                "99C31397C91F",
+                "251B8BC717D0",
+                "3330C5D9A36D",
+                "181C5D75C66D",
+        };
+
+        for (int i = 0; i < 16; i++) {
+            assertEquals(trueResults[i], Utils.binaryToHexString(subKeys[i]));
+        }
+
+    }
+}
